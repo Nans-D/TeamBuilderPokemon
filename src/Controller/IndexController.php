a75014fdbd5ee5ae75dce04f11fe75452ef672da
Nans-D <ndelaubert@gmail.com>
2024-10-24 22:19:16 +0200

    commit final

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use PokePHP\PokeApi;
 
+set_time_limit(0);
+
+
 class IndexController extends AbstractController
 {
     private PokeApi $pokeApi;
@@ -20,7 +23,6 @@ class IndexController extends AbstractController
     public function index(int $id): Response
     {
 
-
         $user = $this->getUser();  // Vérifie si l'utilisateur est connecté
 
         $directory = $this->getParameter('kernel.project_dir') . '/public/data';
@@ -30,16 +32,35 @@ class IndexController extends AbstractController
         // Appel de la fonction de traitement pour chaque cas (version de Pokédex)
         switch ($id) {
             case 1:
-                $responseJson = $this->processPokedex(2, $file, $directory);
+                $version = [
+                    'yellow',
+                    'blue',
+                    'red'
+                ];
+                $responseJson = $this->processPokedex(2, $file, $directory, $version);
                 break;
             case 2:
-                $responseJson = $this->processPokedex(3, $file, $directory);
+                $version = [
+                    'gold',
+                    'silver',
+                    'crystal'
+                ];
+                $responseJson = $this->processPokedex(3, $file, $directory, $version);
                 break;
             case 3:
-                $responseJson = $this->processPokedex(4, $file, $directory);
+                $version = [
+                    'ruby',
+                    'sapphire',
+                    'emerald'
+                ];
+                $responseJson = $this->processPokedex(4, $file, $directory, $version);
                 break;
             case 4:
-                $responseJson = $this->processPokedex(5, $file, $directory);
+                $version = [
+                    'firered',
+                    'leafgreen'
+                ];
+                $responseJson = $this->processPokedex(5, $file, $directory, $version);
                 break;
             default:
                 throw $this->createNotFoundException("Pokedex version not found");
@@ -54,7 +75,7 @@ class IndexController extends AbstractController
     /**
      * Fonction pour récupérer et traiter les données du Pokédex.
      */
-    private function processPokedex(int $pokedexId, string $file, string $directory): ?string
+    private function processPokedex(int $pokedexId, string $file, string $directory, array $version): ?string
     {
         // Vérifie si le fichier existe déjà
         if (file_exists($file)) {
@@ -68,6 +89,7 @@ class IndexController extends AbstractController
 
         // Boucle sur chaque entrée du Pokédex pour traiter les détails des Pokémon
         foreach ($pokemonData['pokemon_entries'] as $name) {
+
             $responsePokemonName = $this->pokeApi->pokemon($name['pokemon_species']['name']);
             $pokemonDetails = json_decode($responsePokemonName, true);
 
@@ -115,12 +137,28 @@ class IndexController extends AbstractController
                 continue;
             }
 
+            switch ($pokedexId) {
+                case 2:
+                    $encounterArea = $this->getLocationVersion($name['pokemon_species']['name'], $version);
+                    break;
+                case 3:
+                    $encounterArea = $this->getLocationVersion($name['pokemon_species']['name'], $version);
+                    break;
+                case 4:
+                    $encounterArea = $this->getLocationVersion($name['pokemon_species']['name'], $version);
+
+                    break;
+                case 5:
+                    $encounterArea = $this->getLocationVersion($name['pokemon_species']['name'], $version);
+                    break;
+            }
 
             $imagesPokemonArray[] = [
                 'id' => $pokemonDetails['id'],
                 'name' => $name['pokemon_species']['name'],
                 'image' => $pokemonDetails['sprites']['front_default'] ?? null, // Utilise l'image par défaut s'il y en a une
-                'types' => $typesCompiled
+                'types' => $typesCompiled,
+                'encounters' => $encounterArea,
             ];
         }
 
@@ -133,5 +171,38 @@ class IndexController extends AbstractController
         file_put_contents($file, json_encode($imagesPokemonArray, JSON_PRETTY_PRINT));
 
         return json_encode($imagesPokemonArray);
+    }
+
+    public function getLocationVersion($pokemonName, $version): array
+    {
+        $encounterArea = [];
+        foreach (json_decode($this->pokeApi->pokemon($pokemonName . '/encounters'), true) as $encounter) {
+
+            foreach ($encounter['version_details'] as $versionDetails) {
+                $versionName = $versionDetails['version']['name'];
+                $locationName = $encounter['location_area']['name'];
+
+                // Ajouter uniquement pour les versions spécifiées
+                if (in_array($versionName, $version)) {
+                    // Vérifier si la version existe déjà dans le tableau
+                    if (!isset($encounterArea[$versionName])) {
+                        // Créer un nouvel objet pour cette version
+                        $encounterArea[$versionName] = [
+                            'version' => $versionName,
+                            'locations' => []
+                        ];
+                    }
+
+                    // Ajouter le nom de la localisation à l'objet de cette version
+                    if (!in_array($locationName, $encounterArea[$versionName]['locations'])) {
+                        $encounterArea[$versionName]['locations'][] = $locationName;
+                    }
+                }
+            }
+        }
+
+        // Réindexer le tableau pour qu'il soit un tableau d'objets
+        $encounterArea = array_values($encounterArea);
+        return $encounterArea;
     }
 }
